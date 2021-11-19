daf2fa881535860b81b2e1d6ec16c1d024b09551
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-19 18:43:24 -0500

    Creating Test with MockMvn and Expect Results

---
+++ src/test/java/com/bharath/springweb/controllers/ProductControllerTest.java
@@ -0,0 +1,47 @@
+package com.bharath.springweb.controllers;
+
+import com.bharath.springweb.entities.Product;
+import com.bharath.springweb.repositories.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductRepository repository;
+
+    @Test
+    void getProducts() throws Exception {
+
+        Product product = new Product();
+        product.setId(1);
+        product.setName("MacBook");
+        product.setDescription("Its Awesome");
+        product.setPrice(5000);
+
+        List<Product> products = Arrays.asList(product);
+        when(repository.findAll()).thenReturn(products);
+
+        mockMvc.perform(get("/productapi/products/").contextPath("/productapi"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[{'id':1, 'name':'MacBook', 'description':'Its Awesome', 'price':5000}]"));
+    }
+
+
+}
