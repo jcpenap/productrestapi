38334c0357ae30c2af13bdcb2b51fbe12ef8e57e
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-22 18:05:39 -0500

    Adding Info to Swagger Api UI

--- src/main/java/com/bharath/springweb/config/SwaggerConfig.java
+++ src/main/java/com/bharath/springweb/config/SwaggerConfig.java
@@ -2,8 +2,10 @@ package com.bharath.springweb.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -12,9 +14,20 @@ public class SwaggerConfig {
 
     @Bean
     public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(getApiInfo()).select()
                 .apis(RequestHandlerSelectors.basePackage("com.bharath.springweb.controllers"))
                 .paths(PathSelectors.regex("/productapi/products.*")).build();
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Product API")
+                .description("Product CRUD Operations")
+                .termsOfServiceUrl("Open Source")
+                .license("Bahrath License")
+                .licenseUrl("bharaththippireddy.com")
+                .version("2.0")
+                .build();
     }
 
 }
